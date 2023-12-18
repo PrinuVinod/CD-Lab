231aea468eeb03d2db166026186200bdcaa35885
Prinu Vinod Nair <71308351+PrinuVinod@users.noreply.github.com>
2023-12-18 18:34:11 +0530

    updated epsilon

--- C/Epsilon Closure/epsilon.c
+++ C/Epsilon Closure/epsilon.c
@@ -3,21 +3,25 @@
 
 char result[20][20],states[20][20];
 
-void add_state(char a[3],int i){
+void add_state(char a[3],int i)
+{
 	strcpy(result[i],a);
 }
 
-void display(int n, char states[3]){
+void display(int n, char states[3])
+{
 	int k=0;
 	printf("\n\n\n Epsilon closure of %s = { ",states);
-	while(k < n){
+	while(k < n)
+	{
 		printf(" %s",result[k]);
 		k++;
 	}
 	printf(" } \n\n\n");
 }
 
-int main(){
+int main()
+{
     FILE *INPUT;
     INPUT=fopen("input.txt","r");
     int end,i=0,n,k=0;
@@ -25,34 +29,36 @@ int main(){
     printf("\n Enter the no of states: ");
     scanf("%d",&n);
     printf("\n Enter the states: \n");
-    for(k=0;k<3;k++){
+    for(k=0;k<3;k++)
+	{
         printf("%d: ", k+1);
 		scanf("%s",states[k]);
 	}
 	
-	for( k=0;k<n;k++){
+	for( k=0;k<n;k++)
+	{
 		i=0;
 		strcpy(state,states[k]);
 		strcpy(result[i++], state);
 		while(1){
 			end = fscanf(INPUT,"%s%s%s",state1,input,state2);
-			if (end == EOF ){
+			if (end == EOF )
+			{
 				break;
 			}
             printf("\n1: %s, 2: %s, 3: %s", state1, input, state2);
-
-			if( strcmp(state,state1) == 0 ){
-				if( strcmp(input,"e") == 0) {
+			if( strcmp(state,state1) == 0 )
+			{
+				if( strcmp(input,"e") == 0)
+				{
                     printf("\n %s is e of %s", state2, state);
 					if(strcmp(state2, states[k]) != 0) strcpy(result[i++], state2);
 					strcpy(state, state2);
 				}
 			}
-
 		}
 		display(i, states[k]);
 		rewind(INPUT);
 	}
-  
     return 0;
 }
