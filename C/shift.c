0608e059216f079b2703757c20fa9104c640faef
Prinu Vinod Nair <71308351+PrinuVinod@users.noreply.github.com>
2023-12-19 01:03:57 +0530

    updated shift code

--- C/shift.c
+++ C/shift.c
@@ -1,60 +1,94 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int z=0,i=0,j=0,c=0;
-char inputString[16],stk[15];
+int z = 0, i = 0, j = 0, c = 0;
+char inputString[16], stk[15];
 
 void check();
 void replaceandprint();
 
 void main()
 {
+	// Display the given grammar rules
 	printf("\nGRAMMAR :\n E->E+E \n E->E*E \n E->E/E \n E->a\n E->b\n");
 
+	// Prompt the user to enter an input string
 	puts("Enter input string: ");
 	gets(inputString);
 
-	c=strlen(inputString);
+	// Get the length of the input string
+	c = strlen(inputString);
 
+	// Display the table headers
 	puts("Stack \t Input \t\t Action");
-	for(i=0, j=0; j<c; i++,j++) {
-		stk[i]=inputString[j];
-		stk[i+1]='\0';
-		inputString[j]=' ';
-		printf("\n$%s\t%s$\t%s %c",stk,inputString,"SHIFT", stk[i]);
+
+	// Loop through the input string
+	for (i = 0, j = 0; j < c; i++, j++)
+	{
+		// Push the current character onto the stack
+		stk[i] = inputString[j];
+		stk[i + 1] = '\0';
+		inputString[j] = ' ';
+
+		// Display the current stack, input, and the action (SHIFT)
+		printf("\n$%s\t%s$\t%s %c", stk, inputString, "SHIFT", stk[i]);
+
+		// Check if any reduction is possible
 		check();
 	}
+
 	printf("\n\n");
 
-	if(strcmp(stk,"E")==0) {
+	// Check if the final stack contains only 'E'
+	if (strcmp(stk, "E") == 0)
+	{
 		printf("The input is valid and accepted\n\n");
-	} else
+	}
+	else
+	{
 		printf("Invalid Input\n\n");
+	}
 }
 
-void check() {
-	for(z=0; z<c; z++) {
-		if(stk[z]=='a' || stk[z]=='b') {
+void check()
+{
+	// Check for terminals 'a' or 'b' in the stack
+	for (z = 0; z < c; z++)
+	{
+		if (stk[z] == 'a' || stk[z] == 'b')
+		{
+			// Replace terminals 'a' or 'b' with 'E' and print the reduction
 			replaceandprint();
-			i = i+2;
+			i = i + 2;
 			break;
 		}
 	}
-	for(z=0; z<c; z++) {
-		if((stk[z]=='E' && stk[z+1]=='+' && stk[z+2]=='E') ||
-		   (stk[z]=='E' && stk[z+1]=='*' && stk[z+2]=='E') ||
-		   (stk[z]=='E' && stk[z+1]=='/' && stk[z+2]=='E') ||
-		   (stk[z]=='(' && stk[z+1]=='E' && stk[z+2]==')') ) {
+
+	// Check for productions in the stack and replace them with 'E'
+	for (z = 0; z < c; z++)
+	{
+		if ((stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'E') ||
+			(stk[z] == 'E' && stk[z + 1] == '*' && stk[z + 2] == 'E') ||
+			(stk[z] == 'E' && stk[z + 1] == '/' && stk[z + 2] == 'E') ||
+			(stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')'))
+		{
+			// Replace the production with 'E' and print the reduction
 			replaceandprint();
 		}
 	}
 }
 
-void replaceandprint(){
-	stk[z]='E';
-	stk[z+1]='\0';
-	stk[z+2]='\0';
-	printf("\n$%s\t%s$\t%s",stk,inputString,"REDUCE TO E");
-	i=i-2; 
+void replaceandprint()
+{
+	// Replace part of the stack with 'E'
+	stk[z] = 'E';
+	stk[z + 1] = '\0';
+	stk[z + 2] = '\0';
+
+	// Display the current stack, input, and the action (REDUCE TO E)
+	printf("\n$%s\t%s$\t%s", stk, inputString, "REDUCE TO E");
+
+	// Adjust the index to account for the reduction
+	i = i - 2;
 }
